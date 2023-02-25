c496aa7e412210d30d0dfd2eef855c36cec7ee97
Andrei Malets <coolviktori@yandex.by>
2023-02-25 22:58:38 +0300

    test

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export default class PostService {
     static async getAll(){
         try{
-            // const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+            // const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
             const response = await axios.get('https://blog-platform-for-guild.vercel.app/blogs')
         return response.data
         }
