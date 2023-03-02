24863e10b5dd8deafbe0e691d99dddd5bbcf7969
Andrei Malets <coolviktori@yandex.by>
2023-03-02 23:08:01 +0300

    testing

---
+++ src/components/UI/Test/TypeScrTestsList.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TypeScrTestsList = () => {
+    const tasks=[
+        {id:1,  title: 'JS', isDone: false},
+        {id:2,  title: 'Java', isDone: false},
+        {id:3,  title: 'NextJS', isDone: false},
+        {id:4,  title: 'Node.JS', isDone: false}
+    ]
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default TypeScrTestsList
