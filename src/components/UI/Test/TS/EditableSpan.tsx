dcd9bdc19fa328bde7781f30f22190ead5ddd72a
Andrei Malets <coolviktori@yandex.by>
2023-03-09 22:14:19 +0300

    changes EditableSpan

--- src/components/UI/Test/TS/EditableSpan.tsx
+++ src/components/UI/Test/TS/EditableSpan.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 
-const EditableSpan = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+type EditableSpanType ={
+    value: string
+    onChange: (newValue: string)=>void
 }
+
+const EditableSpan =React.memo( (props:EditableSpanType) => {
+    let [editMode, setEditMode]=React.useState<boolean>(false)
+    let[title, setTitle]=React.useState<string>(props.value)
+    const activateEditMode=()=>{
+        setEditMode(true)
+        setTitle(props.value)
+    }
+    const activateViewMode=()=>{
+        setEditMode(false)
+        props.onChange(title)
+    }
+    const changeTitle=(e:ChangeEvent<HTMLInputElement>)=>{
+        setTitle(e.currentTarget.value)
+    }
+    return (
+        <>
+        {
+            editMode ?
+            <input value={title} onChange={changeTitle} />
+            :
+            <span onDoubleClick={activateEditMode}>{props.value}</span>
+        }
+        
+        </>
+    )
+})
 
 export default EditableSpan
