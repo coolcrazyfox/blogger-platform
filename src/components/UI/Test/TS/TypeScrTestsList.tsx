b9fc3a8e98ebc87c8204b92713fa3829abf7edd7
Andrei Malets <coolviktori@yandex.by>
2023-03-09 21:38:58 +0300

    changes TS

--- src/components/UI/Test/TS/TypeScrTestsList.tsx
+++ src/components/UI/Test/TS/TypeScrTestsList.tsx
@@ -58,6 +58,7 @@ const TypeScrTestsList = React.memo((props: PropsType) => {
         <EditableSpan value={props.title} onChange={changeTodolistTitle}/>
         <button onClick={removeTodolist}>delete</button>
       </h2>
+      <AddItemForm addItem={addTask}/>
 
       
         <TestItem           
