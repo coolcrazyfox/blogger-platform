2cfb806d8333f774005370c3e75c99a9e6a0d127
Andrei Malets <coolviktori@yandex.by>
2023-03-09 22:24:31 +0300

    changes AddItem

--- src/components/UI/Test/TS/AddItemForm.tsx
+++ src/components/UI/Test/TS/AddItemForm.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
-const AddItemForm = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+type AddItemFormType={
+    addItem:(title:string)=>void
 }
+
+const AddItemForm = React.memo( (props:AddItemFormType) => {
+    let[title, setTitle]=React.useState<string>('')
+    let[error, setError]=React.useState<string |null>(null)
+    const addItem =()=>{
+        if(title.trim() !==''){
+            props.addItem(title)
+            setTitle('')
+        }else{
+            setError('Title is required ')
+        }
+    }
+    return (
+        <>
+        
+        </>
+    )
+})
 
 export default AddItemForm
