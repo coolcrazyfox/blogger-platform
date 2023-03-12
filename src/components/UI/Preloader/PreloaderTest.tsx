4b4a981a5d30ec28337c44db67372eb0d91d6fd5
Andrei Malets <coolviktori@yandex.by>
2023-03-12 23:45:43 +0300

    create Preloader

---
+++ src/components/UI/Preloader/PreloaderTest.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+//@ts-ignore
+import st from './PreloaderTest.module.css'
+
+function PreloaderTest() {
+  return (
+    <div className={st.loader}>
+
+    </div>
+  )
+}
+
+export default PreloaderTest
