39c3c569c8cf495a97e9fa8dd3dbd18b0618e538
Andrei Malets <coolviktori@yandex.by>
2023-03-18 23:23:47 +0300

    changes Comment.tsx

--- src/components/UI/Test/UI/Comments/CommentItem.tsx
+++ src/components/UI/Test/UI/Comments/CommentItem.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { CommentType } from '../../../../../redux/CommentsReducer';
+import Button from '../Button/Button';
+import { EditableSpan } from '../EditableSpan/EditableSpan';
+//@ts-ignore
+import st from './Comment.module.css'
 
-const CommentItem = () => {
+
+
+type CommentItemType = {
+    comment: CommentType
+}
+const CommentItem = ({comment, ...props}: CommentItemType) => {
   return (
     <div>
+        <img className={st.avatar} src="https://w7.pngwing.com/pngs/862/646/png-transparent-beard-hipster-male-man-avatars-xmas-giveaway-icon-thumbnail.png" alt="avatar" />
+            <span className={st.name}>{comment.commentatorInfo.userLogin}</span>
+            <span className={st.date}>{comment.createdAt}</span>
+            <div>
+                <EditableSpan title={comment.content} changeTitle={() => { }} />
+            </div>
+            <Button disabled={false} title={'Remove'} onClick={() => { }} />
       
     </div>
   )
