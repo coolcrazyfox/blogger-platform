20dc7354c790d2d27a97409cd50ec52912967fda
Andrei Malets <coolviktori@yandex.by>
2023-03-22 23:08:46 +0300

    changes BlogsForm.tsx

--- src/components/UI/Test/UI/BlogsBox/BlogsForm.tsx
+++ src/components/UI/Test/UI/BlogsBox/BlogsForm.tsx
@@ -13,8 +13,6 @@ type BlogsFormPropsType ={
 const BlogsForm = ({...props}:BlogsFormPropsType) => {
     const [modalActive, setModalActive] = useState<boolean>(false);
     const [disabled, setDisable] = useState<boolean>(false)
-
-
     const {
         register, handleSubmit, formState: { errors }, formState, reset } = useForm({
             mode: 'onBlur',
@@ -36,32 +34,26 @@ const BlogsForm = ({...props}:BlogsFormPropsType) => {
     }
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-        <div className={st.titleInput}>
-            Name
-            <Input 
-                placeholder='name'
-                className={st.inputForm}
-                {...register('name', {
+        <div className={st.titleInput}>Name
+            <Input placeholder='name'  className={st.inputForm} {...register('name', {
                                 required: 'field is required',
                                 maxLength: { value: 15, message: 'Max Length 15' },
-                            })
-                } 
-            />                            
+                            })} />                            
         </div>
-                        <div>{errors?.name && <p>{errors.name.message || 'Error'}</p>}</div>
-                        <div className={st.titleInput}>about
+        <div>{errors?.name && <p>{errors.name.message || 'Error'}</p>}</div>                
+        <div className={st.titleInput}>about
                             <Input placeholder='description' className={st.inputForm} {...register('description', {
                                 required: 'field is required',
                                 maxLength: { value: 500, message: 'Max Length 500' },
                             })} />
-                        </div>
-                        <div>{errors.description && <p>{errors.description.message || 'Error'}</p>}</div>
-                        <div className={st.titleInput}>website
+        </div>                
+        <div>{errors.description && <p>{errors.description.message || 'Error'}</p>}</div>                
+        <div className={st.titleInput}>website
                             <Input placeholder='www.xxx.com' className={st.inputForm} {...register('websiteUrl', { 
                                 required: 'field is required' })} />
-                        </div>
-                        <div>{errors.websiteUrl && <p>{errors.websiteUrl.message || 'Error'}</p>}</div>
-                            <Input  className={st.createBlogButton}  type="submit" value='Create blog' />
+        </div>                
+        <div>{errors.websiteUrl && <p>{errors.websiteUrl.message || 'Error'}</p>}</div>                
+        <Input  className={st.createBlogButton}  type="submit" value='Create blog' />                  
                         
     </form>
   )
