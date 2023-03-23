ca6fdcc118b53d1639e1dced327fa352e306179c
Andrei Malets <coolviktori@yandex.by>
2023-03-23 22:32:03 +0300

    create BlogForm.tsx

---
+++ src/components/UI/Test/UI/BlogsBox/BlogForm.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const BlogForm = () => {
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={st.modalUpdateTitle}>Name
+            <Input  placeholder='Name' className={st.modalInputUpdate} {...register('name', {
+                required: 'field is required',
+                maxLength: { value: 15, message: 'Max Length 15' },
+            })} />
+        </div>
+        <div>{errors?.name && <p>{errors.name.message || 'Error'}</p>}</div>
+        <div className={st.modalUpdateTitle}>about
+            <Input  placeholder='description' className={st.modalInputUpdate} {...register('description', {
+                required: 'field is required',
+                maxLength: { value: 500, message: 'Max Length 500' },
+            })} />
+        </div>
+        <div>{errors.description && <p>{errors.description.message || 'Error'}</p>}</div>
+        <div className={st.modalUpdateTitle}>website
+            <Input  placeholder='www.xxx.com' className={st.modalInputUpdate} {...register('websiteUrl', { 
+                required: 'field is required' })} />
+        </div>
+        <div>{errors.websiteUrl && <p>{errors.websiteUrl.message || 'Error'}</p>}</div>
+        <Input  className={st.updateButton}  type="submit" value='Update Blog' />
+    </form>
+  )
+}
+
+export default BlogForm
