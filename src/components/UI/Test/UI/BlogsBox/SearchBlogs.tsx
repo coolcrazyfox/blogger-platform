4adf92e8f1185efb2e418228e2537e96179e1fae
Andrei Malets <coolviktori@yandex.by>
2023-03-24 22:40:10 +0300

    changes SearchBlogs.tsx

--- src/components/UI/Test/UI/BlogsBox/SearchBlogs.tsx
+++ src/components/UI/Test/UI/BlogsBox/SearchBlogs.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Input } from '../Input/Input'
+//@ts-ignores
+import st from './Blog.module.css'
 
-const SearchBlogs = () => {
+type SearchBlogsPropsType = {
+    searchHandler:()=>void
+    search: string
+}
+
+const SearchBlogs = ({search, searchHandler }:SearchBlogsPropsType) => {
   return (
     <>
-      
+        <Input value={search} onChange={searchHandler} className={st.search} placeholder='search' type="text" />      
     </>
   )
 }
