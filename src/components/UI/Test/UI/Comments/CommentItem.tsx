81fdff863594922c95e5d915830099855306a2a9
Andrei Malets <coolviktori@yandex.by>
2023-04-01 18:15:15 +0300

    changes comments

--- src/components/UI/Test/UI/Comments/CommentItem.tsx
+++ src/components/UI/Test/UI/Comments/CommentItem.tsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import { CommentType } from '../../../../../redux/CommentsReducer';
-import Button from '../Button/Button';
-import { EditableSpan } from '../EditableSpan/EditableSpan';
+import React from "react";
+import { CommentType } from "../../../../../redux/CommentsReducer";
+import Button from "../Button/Button";
+import { EditableSpan } from "../EditableSpan/EditableSpan";
 //@ts-ignore
-import st from './Comment.module.css'
-
-
+import st from "./Comment.module.css";
 
 type CommentItemType = {
-    comment: CommentType
-}
-const CommentItem = ({comment, ...props}: CommentItemType) => {
+  comment: CommentType;
+};
+const CommentItem = ({ comment, ...props }: CommentItemType) => {
   return (
     <div className={st.commentBlock}>
-        <img className={st.avatar} src="https://w7.pngwing.com/pngs/862/646/png-transparent-beard-hipster-male-man-avatars-xmas-giveaway-icon-thumbnail.png" alt="avatar" />
-            <span className={st.name}>{comment.commentatorInfo.userLogin}</span>
-            <span className={st.date}>{comment.createdAt}</span>
-            <div>
-                <EditableSpan title={comment.content} changeTitle={() => { }} />
-            </div>
-            <Button disabled={false} title={'Remove'} onClick={() => { }} />
-      
+      <img
+        className={st.avatar}
+        src="https://w7.pngwing.com/pngs/862/646/png-transparent-beard-hipster-male-man-avatars-xmas-giveaway-icon-thumbnail.png"
+        alt="avatar"
+      />
+      <span className={st.name}>{comment.commentatorInfo.userLogin}</span>
+      <span className={st.date}>{comment.createdAt}</span>
+      <div>
+        <EditableSpan title={comment.content} changeTitle={() => {}} />
+      </div>
+      <Button disabled={false} onClick={() => {}}>
+        Remove
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default CommentItem
+export default CommentItem;
--- src/components/UI/Test/UI/Comments/Comments.tsx
+++ src/components/UI/Test/UI/Comments/Comments.tsx
@@ -1,66 +1,75 @@
-import React, { ChangeEvent, useState } from 'react'
-import { useSelector } from 'react-redux'
-import { useAppDispatch } from '../../../../../store/store'
-import Button from '../Button/Button'
+import React, { ChangeEvent, useState } from "react";
+import { useSelector } from "react-redux";
+import { useAppDispatch } from "../../../../../store/store";
+import Button from "../Button/Button";
 //@ts-ignore
-import st from './Comments.module.css'
-import CommentItem from './CommentItem';
-import { selectComments } from '../../../../../redux/selectors/comments-selectors'
-import { addCommentTC } from '../../../../../redux/CommentsReducer'
+import st from "./Comments.module.css";
+import CommentItem from "./CommentItem";
+import { selectComments } from "../../../../../redux/selectors/comments-selectors";
+import { addCommentTC } from "../../../../../redux/CommentsReducer";
 
 type CommentsType = {
-  postId: string
-}
+  postId: string;
+};
 
-const Comments = ({postId, ...props}:CommentsType) => {
-  const [content,  setContent]= useState<string>('')
-  const [error, setError]= useState<string | null>('')
-  const {page, pageSize, items, pagesCount, totalCount} = useSelector(selectComments)
+const Comments = ({ postId, ...props }: CommentsType) => {
+  const [content, setContent] = useState<string>("");
+  const [error, setError] = useState<string | null>("");
+  const { page, pageSize, items, pagesCount, totalCount } =
+    useSelector(selectComments);
 
-    const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
-   
-    const cancelBtnHandler = () => {
-        setContent('')
+  const cancelBtnHandler = () => {
+    setContent("");
+  };
+
+  const sendCommentHandler = () => {
+    if (content.length > 20 && content.length < 300) {
+      dispatch(addCommentTC({ postId: postId, content: content }));
+      setContent("");
+    } else {
+      setError("Min length 20 and Max length 300");
     }
+  };
 
-    const sendCommentHandler = () => {
-        if (content.length > 20 && content.length < 300) {
-            dispatch(addCommentTC({ postId: postId, content: content }))
-            setContent('')
-        } else {
-            setError('Min length 20 and Max length 300')
-        }
-
-    }
-
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setContent(e.currentTarget.value)
-        setError(null)
-    }
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setContent(e.currentTarget.value);
+    setError(null);
+  };
   return (
     <div className={st.commentsBlock}>
-            <h3 className={st.title}>{`Comments(${totalCount})`}</h3>
-            <input value={content} className={st.comment} onChange={onChangeHandler} type="text" placeholder='Provide your comment...' />
-            {error && <div className={st.error}>{error}</div>}
-            <div>
-                <Button disabled={false} title={'Cancel'} onClick={cancelBtnHandler} />
-                <Button disabled={false} title={'Send your comment'} onClick={sendCommentHandler} />
-            </div>
-            {
-              items.map(c => {
-                return (
-                    <div key={c.id}>
-                        <CommentItem comment={c} />
-                    </div>
-                )
-              }) 
-            }
-            <div className={st.showMore}>
-            <Button disabled={false}  onClick={cancelBtnHandler} >Show more</Button>
-            </div>
-        </div>
-  )
-}
+      <h3 className={st.title}>{`Comments(${totalCount})`}</h3>
+      <input
+        value={content}
+        className={st.comment}
+        onChange={onChangeHandler}
+        type="text"
+        placeholder="Provide your comment..."
+      />
+      {error && <div className={st.error}>{error}</div>}
+      <div>
+        <Button disabled={false} onClick={cancelBtnHandler}>
+          Cancel
+        </Button>
+        <Button disabled={false} onClick={sendCommentHandler}>
+          Send your comment
+        </Button>
+      </div>
+      {items.map((c) => {
+        return (
+          <div key={c.id}>
+            <CommentItem comment={c} />
+          </div>
+        );
+      })}
+      <div className={st.showMore}>
+        <Button disabled={false} onClick={cancelBtnHandler}>
+          Show more
+        </Button>
+      </div>
+    </div>
+  );
+};
 
-export default Comments
+export default Comments;
