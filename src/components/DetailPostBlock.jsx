eab89d132a9546b2735b27e4204254ab40b949f3
Andrei Malets <coolviktori@yandex.by>
2023-04-01 17:58:35 +0300

    changes details

--- src/components/DetailPostBlock.jsx
+++ src/components/DetailPostBlock.jsx
@@ -1,77 +1,68 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import s from "../styles/DetailPostBlock.module.css";
-import Bounce from 'react-reveal/Bounce';
+import Bounce from "react-reveal/Bounce";
 
+const DetailPostBlock = ({ onClickListItem, details, sortDetails }) => {
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState(0);
+  const statePosts = ["for public posts", "for private posts"];
+  const sortName = statePosts[selected];
+  const onClickListItem = (index) => {
+    setSelected(index);
+    setOpen(false);
+  };
 
-const DetailPostBlock = ({onClickListItem, details, sortDetails}) => {
-    const [open, setOpen] = useState(false)
-    const [selected, setSelected] = useState(0)
-    const statePosts = ['for public posts', 'for private posts']
-    const sortName = statePosts[selected]
-    const onClickListenItem = (index) => {
-        setSelected(index)
-        setOpen(false)
-    }
-    console.log('set', setSelected)
+  const rootClasses = [s.lis];
+  if (open) {
+    rootClasses.push(s.active);
+  }
 
-    const rootClasses=[s.lis]
-    if(open){
-        rootClasses.push(s.active)
-    }
+  return (
+    <div className={s.blogs}>
+      <div className={s.box}>
+        <div className={s.container}>
+          <div className={s.post_info_box}>
+            <img src={sortDetails.image} alt="img" />
+            <p>Back-end blog</p>
+          </div>
+          <div className={s.title}>
+            <h2>First day at the office</h2>
+            <span onClick={() => setOpen(!open)}>({sortName})</span>
+          </div>
+          {open && (
+            <Bounce top>
+              <div className={s.select_box}>
+                <ul>
+                  {statePosts.map((name, i) => (
+                    <li
+                      key={i}
+                      onClick={() => onClickListItem(i)}
+                      // onChange={(i)=>setSelected(i)}
+                      className={!selected ? rootClasses.join(" ") : ""}
+                      // className={selected === i ? s.lisActive : s.lis}
+                    >
+                      {name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </Bounce>
+          )}
 
-    return (
-        <div className={s.blogs}>
-            <div className={s.box}>
-                <div className={s.container}>
-                    <div className={s.post_info_box}>
-                        <img src={sortDetails.image} alt="img"/>
-                        <p>Back-end blog</p>
-                    </div>
-                    <div className={s.title}>
-                        <h2>
-                            First day at the office
-                        </h2>
-                        <span onClick={() => setOpen(!open)}>({sortName})</span>
-                    </div>
-                    {open && (
-                        <Bounce top>
-                            <div className={s.select_box}>
-
-                                <ul>
-                                    {statePosts.map((name, i) => (
-                                        <li
-                                            key={i}
-                                            onClick={() => onClickListenItem(i)}
-                                            // onChange={(i)=>setSelected(i)}
-                                            className={!selected? rootClasses.join(' '):''}
-                                            // className={selected === i ? s.lisActive : s.lis}
-                                        >
-                                            {name}
-                                        </li>)
-                                    )}
-
-                                </ul>
-
-                            </div>
-                        </Bounce>
-                    )
-                    }
-
-                    <div className={s.date_info}>
-                        12/12/2022 at 15:46:58
-                    </div>
-                    <div className={s.main_img}>
-                        <img src={sortDetails.image} alt="img"/>
-                    </div>
-                    <textarea className={s.text_form} name="" id="" cols="30" rows="10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dolorum quis repellendus sunt veritatis. Accusamus aperiam architecto blanditiis dolores harum impedit minima non quo saepe totam ullam, vel vero voluptatibus.
-                    </textarea>
-
-                </div>
-            </div>
-
-
+          <div className={s.date_info}>12/12/2022 at 15:46:58</div>
+          <div className={s.main_img}>
+            <img src={sortDetails.image} alt="img" />
+          </div>
+          <textarea className={s.text_form} name="" id="" cols="30" rows="10">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores
+            dolorum quis repellendus sunt veritatis. Accusamus aperiam
+            architecto blanditiis dolores harum impedit minima non quo saepe
+            totam ullam, vel vero voluptatibus.
+          </textarea>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default DetailPostBlock;
--- src/pages/DetailsPost.jsx
+++ src/pages/DetailsPost.jsx
@@ -1,35 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import s from "../styles/DetailsPost.module.css";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import DetailPostBlock from "../components/DetailPostBlock";
 import InfoPostsPanel from "../components/InfoPostsPanel";
-import {initialPosts} from "../pages/PostsPage";
-
+import { initialPosts } from "../pages/PostsPage";
 
 const DetailsPost = () => {
-    const [selected, setSelected]=useState(0)
-    const details = initialPosts
-    const sortDetails=details[selected]
-    const onClickListItem=(i)=>{
-        setSelected(i)
-    }
-    return (
-        <div className={s.main_container}>
-            <Header/>
-            <NavBar/>
-            <InfoPostsPanel
-                sortDetails={sortDetails}
-            />
-            <DetailPostBlock
-                onClickListItem={onClickListItem}
-                details={details}
-                sortDetails={sortDetails}
-            />
-            <Footer/>
-        </div>
-    );
+  const [selected, setSelected] = useState(0);
+  const details = initialPosts;
+  const sortDetails = details[selected];
+  const onClickListItem = (i) => {
+    setSelected(i);
+  };
+  return (
+    <div className={s.main_container}>
+      <Header />
+      <NavBar />
+      <InfoPostsPanel sortDetails={sortDetails} />
+      <DetailPostBlock
+        onClickListItem={onClickListItem}
+        details={details}
+        sortDetails={sortDetails}
+      />
+      <Footer />
+    </div>
+  );
 };
 
 export default DetailsPost;
