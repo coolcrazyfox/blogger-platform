98a55a76f9cc3b799511e41fb1a5fcc71b255832
Andrei Malets <coolviktori@yandex.by>
2023-04-01 23:19:59 +0300

    changes detailsPost

--- src/components/DetailPostBlock.jsx
+++ src/components/DetailPostBlock.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import s from "../styles/DetailPostBlock.module.css";
 import Bounce from "react-reveal/Bounce";
+import { selectAllPosts } from "./../redux/selectors/posts-selectors";
 
 const DetailPostBlock = ({ onClickListItems, details, sortDetails }) => {
   const [open, setOpen] = useState(false);
   const [selected, setSelected] = useState(0);
   const statePosts = ["for public posts", "for private posts"];
+  //   const statePosts = ["for public", "for private", "3"];
   const sortName = statePosts[selected];
   const onClickListItem = (index) => {
     setSelected(index);
@@ -25,31 +27,37 @@ const DetailPostBlock = ({ onClickListItems, details, sortDetails }) => {
             <img src={sortDetails.image} alt="img" />
             <p>Back-end blog</p>
           </div>
-          <div className={s.title}>
-            <h2>First day at the office</h2>
-            <span onClick={() => setOpen(!open)}>({sortName})</span>
+          <div className={s.title_container}>
+            <div className={s.title}>
+              <h2>First day at the office</h2>
+              <span onClick={() => setOpen(!open)}>({sortName})</span>
+            </div>
+            <div className={s.select_form}>
+              <div className={s.date_info}>12/12/2022 at 15:46:58</div>
+              {open && (
+                <Bounce top>
+                  <div className={s.select_box}>
+                    <ul>
+                      {statePosts.map((name, i) => {
+                        return (
+                          <li
+                            key={i}
+                            onClick={() => onClickListItem(i)}
+                            //   onChange={(i) => setSelected(i)}
+                            //   className={!selected ? rootClasses.join(" ") : ""}
+                            className={selected === i ? s.lisActive : s.lis}
+                          >
+                            {name}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                </Bounce>
+              )}
+            </div>
           </div>
-          {open && (
-            <Bounce top>
-              <div className={s.select_box}>
-                <ul>
-                  {statePosts.map((name, i) => (
-                    <li
-                      key={i}
-                      onClick={() => onClickListItem(i)}
-                      //   onChange={(i) => setSelected(i)}
-                      className={!selected ? rootClasses.join(" ") : ""}
-                      //   className={selected === i ? s.lisActive : s.lis}
-                    >
-                      {name}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </Bounce>
-          )}
 
-          <div className={s.date_info}>12/12/2022 at 15:46:58</div>
           <div className={s.main_img}>
             <img src={sortDetails.image} alt="img" />
           </div>
