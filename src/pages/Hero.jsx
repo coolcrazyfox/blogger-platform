5d651d3be217ad9bd4d487c2bf713694af302966
Andrei Malets <coolviktori@yandex.by>
2023-04-06 20:41:24 +0300

    changes Header

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,37 +1,39 @@
-import React from 'react';
-import s from '../styles/Hero.module.css'
+import React from "react";
+import s from "../styles/Hero.module.css";
 import NavBar from "../components/NavBar";
 import Header from "../components/Header";
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from "styled-components";
 
-export const LightTheme={
-    pageBackGround: 'black',
-    titleColor: '#ff9900',
-    tagLineColor: 'black'
-}
-export const DarkTheme={
-    pageBackGround: 'black',
-    titleColor: 'white',
-    tagLineColor: 'lavender'
-}
- export const themes={
-    light: LightTheme,
-    dark: DarkTheme,
-}
+export const LightTheme = {
+  pageBackGround: "black",
+  titleColor: "#ff9900",
+  tagLineColor: "black",
+};
+export const DarkTheme = {
+  pageBackGround: "black",
+  titleColor: "white",
+  tagLineColor: "lavender",
+};
+export const themes = {
+  light: LightTheme,
+  dark: DarkTheme,
+};
 
 const Hero = () => {
-    //navBar
-    const [isActive, setIsActive] = React.useState(false)  
-    //theme
-    const [theme, setTheme] = React.useState('light')
-    return (
-        <div className={theme==='light'? s.main_container: s.main_container_dark}>
-            <ThemeProvider theme={themes[theme]}>
-                <Header theme={theme} setTheme={setTheme}/>
-            </ThemeProvider>            
-            <NavBar theme={theme}  isActive={isActive} setIsActive= {setIsActive}/>
-        </div>
-    );
+  //navBar
+  const [isActive, setIsActive] = React.useState(false);
+  //theme
+  const [theme, setTheme] = React.useState("light");
+  return (
+    <div
+      className={theme === "light" ? s.main_container : s.main_container_dark}
+    >
+      <ThemeProvider theme={themes[theme]}>
+        <Header theme={theme} setTheme={setTheme} />
+      </ThemeProvider>
+      <NavBar theme={theme} isActives={isActive} setIsActives={setIsActive} />
+    </div>
+  );
 };
 
 export default Hero;
