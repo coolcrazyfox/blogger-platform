48f20413e1074f3cbe904f30add2cb8f2c4eaa18
Andrei Malets <coolviktori@yandex.by>
2023-04-06 20:39:53 +0300

    changes App

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import {HashRouter} from "react-router-dom";
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
     <Provider store={store}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <App />
     </Provider>
   </HashRouter>
 );
-
-
