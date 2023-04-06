cbb668ed5f7c7299cfb68cba4aa145574f66c0bb
Andrei Malets <coolviktori@yandex.by>
2023-04-06 22:38:18 +0300

    changes App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import RouterRoot from "./routes/RouterRoot";
 import "./App.css";
 
-export const AppContext = React.createContext<any>("");
 function App() {
   return (
     <div className={"App"}>
